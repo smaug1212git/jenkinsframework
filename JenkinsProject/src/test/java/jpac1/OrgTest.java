ba8e5498e87683277c3dfb659d7f4f269381876c
smaug12git <samiullah12ahmed@gmail.com>
2025-04-03 14:47:54 +0530

    modified orgtest

--- JenkinsProject/src/test/java/jpac1/OrgTest.java
+++ JenkinsProject/src/test/java/jpac1/OrgTest.java
@@ -8,6 +8,18 @@ public class OrgTest {
 	public void createOrg()
 	{
 		System.out.println("Execution of createOrg");
+		
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		
 	}
 	
 	@Test
