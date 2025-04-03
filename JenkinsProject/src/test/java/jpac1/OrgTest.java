8cc574020d4b4132cff55cfe89ecccb7759dd2e6
smaug12git <samiullah12ahmed@gmail.com>
2025-04-03 12:31:01 +0530

    new push

---
+++ JenkinsProject/src/test/java/jpac1/OrgTest.java
@@ -0,0 +1,25 @@
+package jpac1;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createOrg()
+	{
+		System.out.println("Execution of createOrg");
+	}
+	
+	@Test
+	public void editOrg()
+	{
+		System.out.println("Execution of editOrg");
+	}
+	
+	@Test
+	public void deleteOrg()
+	{
+		System.out.println("Execution of deleteOrg");
+	}
+
+}
---
+++ JenkinsProject/src/test/java/jpac2/ContactTest.java
@@ -0,0 +1,25 @@
+package jpac2;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContact()
+	{
+		System.out.println("Execution of createContact");
+	}
+	
+	@Test
+	public void editcontact()
+	{
+		System.out.println("Execution of editcontact");
+	}
+	
+	@Test
+	public void deletecontac()
+	{
+		System.out.println("Execution of deletecontact");
+	}
+
+}
